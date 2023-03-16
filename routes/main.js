57476db8bc4be5cf309793deeca81e7cb61023cd
Ubaid <rehmanubaid5@gmail.com>
2023-03-16 16:50:55 +0800

    Main route reset

--- routes/main.js
+++ routes/main.js
@@ -2,13 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-const adminController = require('../controller/admin');
-const entryController = require('../controller/entries');
 
 const multer = require('multer');
-
-router.get('/', adminController.Home);
-
 
 
 module.exports = router;
