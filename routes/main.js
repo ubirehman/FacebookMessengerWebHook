be0fab76881b5d83ca7ec28ef94011b342faa806
Ubaid <rehmanubaid5@gmail.com>
2023-03-16 17:37:20 +0800

    Added facebook webhook

--- routes/main.js
+++ routes/main.js
@@ -6,7 +6,13 @@ const router = express.Router();
 const adminController = require('../controller/admin');
 
 
+
+
+
+
 router.get('/', adminController.Home);
+router.get('/webhook', adminController.getWebhook);
+router.get('/webhook', adminController.postWebhook);
 
 
 
