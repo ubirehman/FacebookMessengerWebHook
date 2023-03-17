dfc87209fc017b3f10856edb8c6259080a3a9928
Ubaid <rehmanubaid5@gmail.com>
2023-03-17 16:27:47 +0800

    Json parser

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const
+  express = require('express'),
+  bodyParser = require('body-parser');
 const session = require('express-session');
 
 
@@ -12,14 +13,14 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }));
- 
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const mainRoute = require('./routes/main');
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(mainRoute);
